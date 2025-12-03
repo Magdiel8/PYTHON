f6958b0b75c9de834e6ce86229b61ed832ac00a6
Magdiel8 <magdielnovoasuarez@gmail.com>
2025-12-03 15:12:01 +0000

    jk

---
+++ Novoa_Suarez_Magdiel_PO_3.py
@@ -0,0 +1,3 @@
+clasificaiones={  "Ana": 8.5,"Luis": 7.0, "María": 9.2, "Carlos": 6.8  }
+try:
+
--- Novoa_Suarez_magdiel_42.py
+++ Novoa_Suarez_magdiel_42.py
@@ -1,22 +1,52 @@
 import os
 import platform
-#Mostrar información del sistema operativo
+
 def mostrar_info_sistema():
     sistema = platform.system()
     version = platform.version()
     print(f"Sistema Operativo: {sistema}")
     print(f"Versión del Sistema Operativo: {version}")
-#Guardar y mostrar el directorio de trabajo actual.
+
 def mostrar_directorio_actual():
     directorio_actual = os.getcwd()
     print(f"Directorio de trabajo actual: {directorio_actual}")
-#Crear un directorio principal llamado "proyecto".
+
 def crear_directorio():
     os.makedirs("proyecto", exist_ok=True)
     print("Directorio 'proyecto' creado.")
-#Entrar al directorio "proyecto" y mostrar la nueva ruta actual.
-#Dentro de "proyecto" crear dos subdirectorios:◦ "docs"◦ "src"
-#Entrar en "src" y crear un subdirectorio llamado "modulos".
-#Volver al directorio original desde donde se ejecutó el programa.
-#Realizar un listado recursivo del directorio "proyecto", usando:◦ dir /s en Windows◦ ls -lR en Linux o macOS
 
+def entrar_directorio():
+    os.chdir("proyecto")
+    print(f"Entrado al directorio: {os.getcwd()}")
+
+def crear_subdirectorios():
+    os.makedirs("docs", exist_ok=True)
+    os.makedirs("src", exist_ok=True)
+    print("Subdirectorios 'docs' y 'src' creados en 'proyecto'.")
+
+def crear_modulos():
+    os.chdir("src")
+    os.makedirs("modulos", exist_ok=True)
+    print("Subdirectorio 'modulos' creado en 'src'.")
+
+def volver_directorio():
+    os.chdir("..")  
+    print(f"Volviendo al directorio: {os.getcwd()}")
+
+def listar_directorio():
+    sistema = platform.system()
+    if sistema == "Windows":
+        os.system("dir /s")
+    else:
+        os.system("ls -lR")    
+def main():
+    mostrar_info_sistema()
+    mostrar_directorio_actual()
+    crear_directorio()
+    entrar_directorio()
+    crear_subdirectorios()
+    crear_modulos()
+    volver_directorio()
+    listar_directorio()
+if __name__ == "__main__":
+    main()
